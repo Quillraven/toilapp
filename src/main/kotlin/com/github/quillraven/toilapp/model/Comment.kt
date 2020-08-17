541f34f64b16b66609e45aff81a9e8ee971910d3
Quillraven <quillraven@gmail.com>
2020-08-17 19:23:12 +0200

    add Document annotation for mongodb

--- src/main/kotlin/com/github/quillraven/toilapp/model/Comment.kt
+++ src/main/kotlin/com/github/quillraven/toilapp/model/Comment.kt
@@ -1,7 +1,9 @@
 package com.github.quillraven.toilapp.model
 
+import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
+@Document
 data class Comment(
     val user: User,
     val date: Date,
--- src/main/kotlin/com/github/quillraven/toilapp/model/User.kt
+++ src/main/kotlin/com/github/quillraven/toilapp/model/User.kt
@@ -1,5 +1,8 @@
 package com.github.quillraven.toilapp.model
 
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
 data class User(
     val id: String,
     val name: String,
