6834c20860deb85a7ba844fe583a2c47c7c72095
Quillraven <quillraven@gmail.com>
2020-08-17 19:24:15 +0200

    add web configuration for cross origin calls

---
+++ src/main/kotlin/com/github/quillraven/toilapp/configuration/WebConfiguration.kt
@@ -0,0 +1,18 @@
+package com.github.quillraven.toilapp.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@EnableWebFlux
+class WebConfiguration : WebFluxConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+}
