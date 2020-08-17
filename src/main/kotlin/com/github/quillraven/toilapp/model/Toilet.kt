d9e56df112fe8a4b949ef231b7799010a5aba44c
Quillraven <quillraven@gmail.com>
2020-08-17 19:22:53 +0200

    add default values and equals/hashcode for correct behavior with comments/images array

--- src/main/kotlin/com/github/quillraven/toilapp/model/Toilet.kt
+++ src/main/kotlin/com/github/quillraven/toilapp/model/Toilet.kt
@@ -1,11 +1,32 @@
 package com.github.quillraven.toilapp.model
 
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
 data class Toilet(
-    val title: String,
-    val description: String,
-    val preview: String,
-    val rating: Int,
-    val comments: Array<Comment>,
-    val location: String,
-    val images: Array<String>
-)
+    val title: String = "",
+    val description: String = "",
+    val preview: String = "",
+    val rating: Int = 0,
+    val comments: Array<Comment> = arrayOf(),
+    val location: String = "",
+    val images: Array<String> = arrayOf()
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Toilet
+
+        if (title != other.title) return false
+        if (location != other.location) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + location.hashCode()
+        return result
+    }
+}
