90641db5ea4017e2a571a35bae05c0623e411910
Quillraven <quillraven@gmail.com>
2020-08-17 19:21:20 +0200

    replace spring web with spring webflux

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
