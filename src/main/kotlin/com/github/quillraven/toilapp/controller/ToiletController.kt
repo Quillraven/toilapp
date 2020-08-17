03b03b441fab9c7890befea11d2bc0572ec74711
Quillraven <quillraven@gmail.com>
2020-08-17 19:23:34 +0200

    adjust code to webflux

--- src/main/kotlin/com/github/quillraven/toilapp/controller/ToiletController.kt
+++ src/main/kotlin/com/github/quillraven/toilapp/controller/ToiletController.kt
@@ -3,17 +3,17 @@ package com.github.quillraven.toilapp.controller
 import com.github.quillraven.toilapp.model.Toilet
 import com.github.quillraven.toilapp.repository.ToiletRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.data.domain.Example
+import org.springframework.data.domain.Sort
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
-@CrossOrigin(origins = ["http://localhost:8081"])
 @RestController
 @RequestMapping("/api")
 class ToiletController {
@@ -21,25 +21,13 @@ class ToiletController {
     lateinit var toiletRepository: ToiletRepository
 
     @GetMapping("/toilets/{location}")
-    fun getNearbyToilets(@PathVariable("location") location: String): ResponseEntity<Array<Toilet>> {
-        try {
-            val nearbyToilets = toiletRepository.findByLocation(location)
-            if (nearbyToilets.isEmpty()) {
-                return ResponseEntity(HttpStatus.NO_CONTENT)
-            }
-            return ResponseEntity(nearbyToilets, HttpStatus.OK)
-        } catch (e: Exception) {
-            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-        }
-    }
+    fun getNearbyToilets(@PathVariable("location") location: String): Flux<Toilet> =
+        toiletRepository.findAll(
+            Example.of(Toilet(location = location)),
+            Sort.by("location")
+        )
 
     @PostMapping("/toilets")
-    fun createToilet(@RequestBody toilet: Toilet): ResponseEntity<Toilet> {
-        try {
-            val newToilet = toiletRepository.save(toilet.copy())
-            return ResponseEntity(newToilet, HttpStatus.CREATED)
-        } catch (e: Exception) {
-            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-        }
-    }
+    fun createToilet(@RequestBody toilet: Toilet): Mono<Toilet> =
+        toiletRepository.save(toilet)
 }
