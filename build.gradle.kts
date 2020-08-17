3b5065bbb07b9d2ad93e4a3b81c5b57d215dbb6e
Quillraven <quillraven@gmail.com>
2020-08-17 19:33:32 +0200

    raise kotlin version to 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 plugins {
     id("org.springframework.boot") version "2.3.3.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
+    kotlin("plugin.spring") version "1.4.0"
 }
 
 group = "com.github.quillraven"
