7d298f62d266a0fa392f3c18438616d5c1e68ed6
Quillraven <quillraven@gmail.com>
2020-08-23 13:05:11 +0200

    add configureHttpMessageCodes to use custom ObjectMapper with GeoJsonModule

--- backend/src/main/kotlin/com/github/quillraven/toilapp/configuration/WebConfiguration.kt
+++ backend/src/main/kotlin/com/github/quillraven/toilapp/configuration/WebConfiguration.kt
@@ -1,19 +1,37 @@
 package com.github.quillraven.toilapp.configuration
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.config.GeoJsonConfiguration
+import org.springframework.data.mongodb.core.geo.GeoJsonModule
+import org.springframework.data.web.config.SpringDataJacksonModules
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
 @EnableWebFlux
-class WebConfiguration : WebFluxConfigurer, GeoJsonConfiguration() {
+class WebConfiguration : WebFluxConfigurer, SpringDataJacksonModules {
+    private val objectMapper = jacksonObjectMapper().apply {
+        registerModule(GeoJsonModule())
+    }
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:4200")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
+    }
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().jackson2JsonEncoder(
+            Jackson2JsonEncoder(objectMapper)
+        )
+        configurer.defaultCodecs().jackson2JsonDecoder(
+            Jackson2JsonDecoder(objectMapper)
+        )
     }
 }
