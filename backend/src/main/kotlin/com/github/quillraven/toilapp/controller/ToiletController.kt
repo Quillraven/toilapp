161a5ce67acc1350fb7b3f4a571d36bcf7e7093f
klausm <simon.klausner@swarovski.com>
2020-09-01 18:33:39 +0200

    delegate logic of controller to service class and pass in service as constructor parameter for easier testing

--- backend/src/main/kotlin/com/github/quillraven/toilapp/controller/ToiletController.kt
+++ backend/src/main/kotlin/com/github/quillraven/toilapp/controller/ToiletController.kt
@@ -1,26 +1,13 @@
 package com.github.quillraven.toilapp.controller
 
-import com.github.quillraven.toilapp.ToiletDoesNotExistException
 import com.github.quillraven.toilapp.model.Toilet
-import com.github.quillraven.toilapp.repository.ToiletRepository
-import org.slf4j.LoggerFactory
+import com.github.quillraven.toilapp.service.IToiletService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
-
-private val LOG = LoggerFactory.getLogger(ToiletController::class.java)
 
 fun <T : Any> KClass<out T>.getNonNullProperties(vararg exceptions: KProperty1<T, *>) =
     declaredMemberProperties
@@ -30,23 +17,15 @@ fun <T : Any> KClass<out T>.getNonNullProperties(vararg exceptions: KProperty1<T
 
 @RestController
 @RequestMapping("/api")
-class ToiletController {
+class ToiletController(
     @Autowired
-    lateinit var toiletRepository: ToiletRepository
-
+    private val toiletService: IToiletService
+) {
     @PostMapping("/toilets")
-    fun createToilet(@RequestBody toilet: Toilet): Mono<Toilet> {
-        LOG.debug("createToilet: $toilet")
-        return toiletRepository.save(toilet)
-    }
+    fun createToilet(@RequestBody toilet: Toilet) = toiletService.create(toilet)
 
     @PutMapping("/toilets/{id}")
-    fun updateToilet(@PathVariable id: String, @RequestBody toilet: Toilet): Mono<Toilet> {
-        LOG.debug("updateToilet: (id=$id, toilet=$toilet)")
-        return toiletRepository.findById(id)
-            .flatMap { toiletRepository.save(toilet.copy(id = id)) }
-            .switchIfEmpty(Mono.error(ToiletDoesNotExistException(id)))
-    }
+    fun updateToilet(@PathVariable id: String, @RequestBody toilet: Toilet) = toiletService.update(id, toilet)
 
     @GetMapping("/toilets")
     fun getNearbyToilets(
@@ -54,20 +33,16 @@ class ToiletController {
         @RequestParam(required = false) y: Double?,
         @RequestParam(required = false) maxDistanceInMeters: Double?
     ): Flux<Toilet> {
-        LOG.debug("getNearbyToilets: (x=$x, y=$y, maxDistanceInMeters=$maxDistanceInMeters)")
         return when {
             x == null || y == null || maxDistanceInMeters == null -> {
-                toiletRepository.findAll()
+                toiletService.getAll()
             }
             else -> {
-                toiletRepository.getNearbyToilets(x, y, maxDistanceInMeters)
+                toiletService.getNearbyToilets(x, y, maxDistanceInMeters)
             }
         }
     }
 
     @DeleteMapping("/toilets/{id}")
-    fun deleteToilet(@PathVariable id: String): Mono<Void> {
-        LOG.debug("deleteToilet: (id=$id)")
-        return toiletRepository.deleteById(id)
-    }
+    fun deleteToilet(@PathVariable id: String) = toiletService.delete(id)
 }
