7640f3d4235afa2dab8e4d78de6da6a142824386
Quillraven <quillraven@gmail.com>
2020-09-19 21:53:20 +0200

    make preview image conditional

--- frontend/src/components/ToiletCard.tsx
+++ frontend/src/components/ToiletCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,48 +7,50 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Toilet } from '../services/ToiletService';
+import {Toilet} from '../services/ToiletService';
 
 const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
+    root: {
+        maxWidth: 345
+    },
 });
 
 interface ToiletCardProps {
-  toilet: Toilet
+    toilet: Toilet
 }
 
 export default function ToiletCard(props: ToiletCardProps) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image={props.toilet.preview}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.toilet.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.toilet.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Details
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                {props.toilet.previewImage &&
+                <CardMedia
+                    component="img"
+                    alt={props.toilet.title}
+                    height="140"
+                    image={props.toilet.previewImage}
+                    title={props.toilet.title}
+                />
+                }
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.toilet.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.toilet.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+                <Button size="small" color="primary">
+                    Details
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
