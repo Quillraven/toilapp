0eac52d93f034c6e3e331978eeb577de3675b24b
Quillraven <quillraven@gmail.com>
2020-09-19 22:22:32 +0200

    change to RestToiletService and align grid items to bottom

--- frontend/src/components/ToiletList.tsx
+++ frontend/src/components/ToiletList.tsx
@@ -1,62 +1,48 @@
-import React from 'react';
-import { Toilet, MockToiletService, ToiletService } from '../services/ToiletService';
+import React, {useEffect, useState} from 'react';
+import {RestToiletService, Toilet, ToiletService} from '../services/ToiletService';
 import ToiletCard from './ToiletCard';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { GridList, GridListTile } from '@material-ui/core';
-
-interface User {
-    id: string
-    name: string
-    email: string
-}
-
-interface Comment {
-    user: User
-    date: Date
-    text: string
-}
-
-interface ToiletListProps {
-}
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import {GridList, GridListTile} from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      width: 1000
-    },
-    icon: {
-      color: 'rgba(255, 255, 255, 0.54)',
-    },
-  }),
+    createStyles({
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            overflow: 'hidden',
+            backgroundColor: theme.palette.background.paper,
+        },
+        gridList: {
+            alignItems: "baseline",
+            width: 1000
+        },
+        icon: {
+            color: 'rgba(255, 255, 255, 0.54)',
+        },
+    }),
 );
 
 export default function ToiletList() {
     const classes = useStyles();
-    const [toilets, setToilets] = React.useState<Toilet[]>([]);
-    const service: ToiletService = new MockToiletService();
+    const [toilets, setToilets] = useState<Toilet[]>([]);
+    const service: ToiletService = new RestToiletService();
 
-    React.useEffect(() => {
-        const loadToilets = async function() {
-            console.log("load toilets");
-            const toiletList: Toilet[] = await service.getToilets();
-            console.log("toilets loaded");
-            setToilets(toiletList);
-        }
-        loadToilets();
+    useEffect(() => {
+        service
+            .getToilets()
+            .then(toilets => {
+                console.log("Toilet data loaded")
+                setToilets(toilets)
+            })
+        // eslint-disable-next-line
     }, []);
 
     return (
         <GridList cellHeight={350} className={classes.gridList} cols={3}>
             {toilets.map((toilet) => (
                 <GridListTile key={toilet.title}>
-                    <ToiletCard toilet={toilet} />
+                    <ToiletCard toilet={toilet}/>
                 </GridListTile>
             ))}
         </GridList>
