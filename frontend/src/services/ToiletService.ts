61fe50e098a739e6685e8133baca6e63539d164a
Quillraven <quillraven@gmail.com>
2020-09-19 21:54:41 +0200

    add "loadImages" functionality to getToilets function

--- frontend/src/services/ToiletService.ts
+++ frontend/src/services/ToiletService.ts
@@ -1,5 +1,5 @@
-import { API_ENDPOINT } from "./ServiceConstants";
-import axios, { AxiosResponse } from "axios";
+import {API_ENDPOINT} from "./ServiceConstants";
+import axios from "axios";
 
 export interface User {
     id: string
@@ -19,9 +19,11 @@ export interface GeoPoint {
 }
 
 export interface Toilet {
+    id: string
     title: string
     location: GeoPoint
-    preview: string
+    previewID: string
+    previewImage: any
     rating: number
     disable: boolean
     toiletCrewApproved: boolean
@@ -31,31 +33,37 @@ export interface Toilet {
 }
 
 export interface ToiletService {
-    getToilets(): Promise<Toilet[]>; 
+    getToilets(loadImages?: boolean): Promise<Toilet[]>;
 }
 
-export class RestToiletService implements ToiletService  {
-   
-    public getToilets(): Promise<Toilet[]> {
-        return this.mapData<Toilet[]>(axios.request<Toilet[]>({url: API_ENDPOINT + '/toilets'}), resp => resp.data);
-    }
-
-    private mapData<T>(promise: Promise<AxiosResponse<T>>, map: { (resp: AxiosResponse<T>): T } ): Promise<T> {
-        return new Promise(function(resolve, reject) {
-            promise.then(resp => resolve(map(resp)));
-            promise.catch(e => resolve(e));
-        });
+export class RestToiletService implements ToiletService {
+    public getToilets(loadImages: boolean = true): Promise<Toilet[]> {
+        return axios
+            .get(API_ENDPOINT + '/toilets')
+            .then(response => {
+                if (loadImages) {
+                    const toilets: Toilet[] = response.data
+                    toilets
+                        .filter(it => it.previewID)
+                        .forEach(toilet => toilet.previewImage = API_ENDPOINT + `/previews/${toilet.previewID}`)
+                }
+                return response.data
+            }, error => {
+                console.error(`Could not load toilets. Error=${error}`)
+            })
     }
 }
 
 export class MockToiletService implements ToiletService {
     getToilets(): Promise<Toilet[]> {
-       return new Promise<Toilet[]>(function(resolve, reject) {
+        return new Promise<Toilet[]>(function (resolve, reject) {
             const toilets: Toilet[] = [
                 {
+                    id: "1",
                     title: "Beautiful toilet",
                     location: {x: 47.0, y: 16.0},
-                    preview: "/toilet.jpg",
+                    previewID: "/toilet.jpg",
+                    previewImage: "",
                     rating: 4.6,
                     disable: false,
                     toiletCrewApproved: true,
@@ -64,9 +72,11 @@ export class MockToiletService implements ToiletService {
                     images: []
                 },
                 {
+                    id: "2",
                     title: "Dirty toilet",
                     location: {x: 47.0, y: 16.0},
-                    preview: "/toilet2.jpg",
+                    previewID: "/toilet2.jpg",
+                    previewImage: "",
                     rating: 1.3,
                     disable: false,
                     toiletCrewApproved: true,
@@ -75,9 +85,11 @@ export class MockToiletService implements ToiletService {
                     images: []
                 },
                 {
+                    id: "3",
                     title: "Porta Potty",
                     location: {x: 47.0, y: 16.0},
-                    preview: "/toilet.jpg",
+                    previewID: "/toilet.jpg",
+                    previewImage: "",
                     rating: 2.5,
                     disable: false,
                     toiletCrewApproved: true,
@@ -86,9 +98,11 @@ export class MockToiletService implements ToiletService {
                     images: []
                 },
                 {
+                    id: "4",
                     title: "Shit Heaven",
                     location: {x: 47.0, y: 16.0},
-                    preview: "/toilet2.jpg",
+                    previewID: "/toilet2.jpg",
+                    previewImage: "",
                     rating: 2.5,
                     disable: false,
                     toiletCrewApproved: true,
@@ -96,8 +110,8 @@ export class MockToiletService implements ToiletService {
                     comments: [],
                     images: []
                 }
-            ];    
+            ];
             resolve(toilets);
-       });
+        });
     }
 }
