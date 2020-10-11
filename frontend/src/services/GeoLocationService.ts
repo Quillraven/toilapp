d185803687198897cf411e66cbc62ceff1036899
Christian Baumann <christianbaumann@gmx.at>
2020-10-11 14:35:34 +0200

    refs #7 - distance computation

--- frontend/src/services/GeoLocationService.ts
+++ frontend/src/services/GeoLocationService.ts
@@ -9,9 +9,9 @@ export interface GeoLocationService {
 
 export class DefaultGeoLocationService implements GeoLocationService {
     getGeoLocation(): GeoLocation {
-        //on a mobile phone -> Ask the mobile OS for the location
+        // TODO: on a mobile phone -> Ask the mobile OS for the location
 
-        //If not available: on a computer -> Ask a IP to loaction web service for the location (e.g. Google.)
+        // TODO: If not available: on a computer -> Ask a IP to loaction web service for the location (e.g. Google.)
 
         //If not no available: retrun default loaction
         const geoLoation = {
